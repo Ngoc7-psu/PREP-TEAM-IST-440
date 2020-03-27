82d26e9d59deb398746e161aa73c00a9374d73d5
Ngoc7-psu <59721719+Ngoc7-psu@users.noreply.github.com>
2020-03-27 18:04:48 -0400

    Add files via upload

---
+++ IST 440 - Prep team (Ngoc).py
@@ -0,0 +1,35 @@
+# NGOC D TRAN
+# IST 440 - 101
+# TEAM PREP - DEVELOPMENT PHASE
+
+## Mill operating program
+import sys
+import time
+import numpy
+import numpy as np
+import Adafruit_DHT
+import enum
+
+# Variables
+rollerSpeed #RPM
+rollersGap #Milimeters
+grainWeight #Pounds
+
+
+class Milling:
+
+# Adjusting the speed of the rollers
+    def rollersSpeed(self):
+
+# Adjusting the rollers placement
+    def rollersGap(self):
+
+
+
+
+# Emergency killswitch button (ESC)
+    def Killswitch(self):
+
+
+
+
